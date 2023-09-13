98f31a4815f5d14076db9127e70b6f505bdf6f55
safiya-123 <safiyarox008@gmail.com>
2023-09-13 20:51:07 +0530

    added missing frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import { AppProvider } from './Context/App.context';
-import Homepage from './Pages/HomePage';
 import Login from './Pages/LoginPage';
 import ProfilePage from './Pages/ProfilePage';
 import LoginwithToken from './Pages/LoginWithToken.js';
 import {createBrowserRouter} from 'react-router-dom';
 import {RouterProvider} from 'react-router-dom';
 import ProtectedRoute from './Component/ProtectedRoute.js';
+
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Homepage/>,
+    element:<LoginwithToken/>,
   },
   {
     path:"/login",
