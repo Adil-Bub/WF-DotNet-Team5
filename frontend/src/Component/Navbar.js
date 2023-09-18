033eb28eb854fdfd3d22066971ebb9ff409f7c3a
Adil-Bub <adilahmed1090@gmail.com>
2023-09-18 21:19:23 +0530

    loanCard

--- frontend/src/Component/Navbar.js
+++ frontend/src/Component/Navbar.js
@@ -1,17 +1,23 @@
-
+import React from 'react';
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { AppContext } from "../Context/App.context";
+import { useNavigate, Link, NavLink } from 'react-router-dom';
+
 export function NavBar(){
+  const { user, setUser } = useContext(AppContext);
+
     return (
         <>
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href='/dashboard/user/'>LUMA</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href='/dashboard/user/loans'>View</Nav.Link>
-            <Nav.Link href='/dashboard/user/loans/apply'>Apply</Nav.Link>
-            <Nav.Link href='/dashboard/user/items=purchased'>Orders</Nav.Link>
+            <NavLink to='/dashboard/user/loans'>View</NavLink>
+            <NavLink to='/dashboard/user/loans/apply'>Apply</NavLink>
+            <NavLink to='/dashboard/user/items=purchased'>Orders</NavLink>
           </Nav>
         </Container>
       </Navbar>
