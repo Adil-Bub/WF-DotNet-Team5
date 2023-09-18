28dd37e70d9c44b80041a5ee00701db40cbb5616
mmehak004 <mmehak200029@gmail.com>
2023-09-18 14:48:28 +0530

    Added LoanCardPage

--- frontend/src/Pages/LoanCardPage.js
+++ frontend/src/Pages/LoanCardPage.js
@@ -1,25 +1,80 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState,useEffect } from "react";
 import axios from 'axios';
 import { AppContext } from "../Context/App.context";
 import { useNavigate } from 'react-router-dom';
+import { FaEdit, FaTrash } from 'react-icons/fa'
+
 
 const LoanCardPage = () => {
 
     const { user, setUser } = useContext(AppContext);
     const navigate = useNavigate();
+    const [loanCards, setLoanCards] = useState([]);
 
+
+
+
+
+    useEffect(() => {
+        axios
+            .get('https://localhost:7189/api/LoanCard/all', {
+                headers: { 'Authorization': 'Bearer ' + user.token }
+            })
+            .then((response) => {
+                setLoanCards(response.data);
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data: ', error);
+            });
+        }, [user.token]);
    
     return (
-        <div className="text-center">
-            <h1>
-                Loan Management Application  
-            </h1>
-            <h4>
-                Loan Card Data
-            </h4>
-            
-        </div>
-    );
-}
+       
+
+                <div>
+                    <nav className="navbar" style={{ 'background': '#cce6ff' }}>
+                        <h4 className="pl-2">Loan management system: Admin</h4>
+                    </nav>
+                    <div className="container mt-5">
+                        <div className="row justify-content-center">
+                            <div className="table-responsive">
+                                <table className="table table-hover table-bordered">
+                                    <thead className="text-center align-items-center">
+                                        <tr>
+                                            <th className="align-middle">Loan_Id</th>
+                                            <th className="align-middle">Loan_Type</th>
+                                            <th className="align-middle">Duration</th>
+                                            <th className="align-middle">Edit</th>
+                                            <th className="align-middle">Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="table-group-divider text-center">
+                                        {loanCards.map((item) => (
+                                    <tr key={item.loanId}>
+                                        <td>{item.loanId}</td>
+                                        <td>{item.loanType}</td>
+                                        <td>{item.durationInYears}</td>
+                                        <td>
+                                                    <FaEdit className="edit-icon" color="blue" onClick={() => {
+                                                    
+                                                    }}></FaEdit>
+                                                </td>
+                                                <td>
+                                                    <FaTrash className="delete-icon" color="red"></FaTrash>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        
+    
+
 
 export default LoanCardPage;
