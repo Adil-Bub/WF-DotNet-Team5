f71b1606450fdb7401dbc7c253b0231f73a63b96
Mehak <62352153+mmehak004@users.noreply.github.com>
2023-09-18 12:44:19 +0530

    Update Schema-Creation.sql

--- SQLScripts/Schema-Creation.sql
+++ SQLScripts/Schema-Creation.sql
@@ -51,3 +51,9 @@ Employee_id varchar(100) references Employee_master(Employee_id),
 Loan_id varchar(100) references Loan_card_master(Loan_id),
 Card_issue_date DATE default getdate()
 );
+
+INSERT INTO Item_master VALUES
+ (1000001,'Tea Table','Y','Wooden','Furniture',5000),
+  (1000002,'Dining Table','Y','Wooden','Furniture',15000),
+   (1000003,'Dining Set','N','Glass','Crockery',9000),
+    (1000004,'Pen Set','Y','Plastic','Stationary',2000);
