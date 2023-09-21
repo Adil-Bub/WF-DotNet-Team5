bbdf1283adf1c92eacb91fd3285ce7f0fac326ef
yachika30 <yachikankumar@gmail.com>
2023-09-21 10:33:02 +0530

    fixed home page , Made login page as home page

--- frontend/src/Component/Navbar.js
+++ frontend/src/Component/Navbar.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavLink from 'react-bootstrap/esm/NavLink';
+import { AppContext } from "../Context/App.context";
 export function NavBar(){
   const { user, setUser } = useContext(AppContext);
 
