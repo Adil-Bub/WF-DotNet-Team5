69309fdf18e3fa982f39e60ee73da73888ef2ecf
ysd-321 <ysdreplies@gmail.com>
2023-09-27 00:43:37 +0530

    add validation to edit employee modal

--- frontend/src/Component/EditEmployeeModal.js
+++ frontend/src/Component/EditEmployeeModal.js
@@ -1,39 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Modal, Form, Row, Col } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 import axios from 'axios';
+import { useFormik } from 'formik';
+import { editEmployeeSchema } from '../Util/validation';
+import { showErrorToast, showSuccessfulToast } from '../Util/toast';
 
 //Todo validation errors
-const EditEmployeeModal = ({ showModal, handleCloseModal, selectedEmployee, setShowModal }) => {
-    
+const EditEmployeeModal = ({ showModal, handleCloseModal, selectedEmployee }) => {
+
     const baseUrl = 'https://localhost:7189/api';
 
     const storedUser = localStorage.getItem('user');
     const user = storedUser ? JSON.parse(storedUser) : null;
 
-    const [employee, setEmployee] = useState(selectedEmployee);
-
-    useEffect(() => {
-        setEmployee(selectedEmployee);
-    }, [selectedEmployee]);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setEmployee({ ...employee, [name]: value });
-    };
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        axios
-            .put(`${baseUrl}/Employee/${employee.employeeId}`, employee, {
-                headers: { 'Authorization': 'Bearer ' + user.token }
-            })
-            .then((response) => {
-                alert('Successfully edited employee details!');
-                //setShowModal(false);
-            }).catch((error) => {
-                alert('Error editing employee details! ', error);
-            });
-    }
+    const formik = useFormik({
+        initialValues: {
+            employeeId: selectedEmployee.employeeId,
+            employeeName: selectedEmployee.employeeName,
+            designation: selectedEmployee.designation,
+            department: selectedEmployee.department,
+            gender: selectedEmployee.gender,
+            dateOfBirth: selectedEmployee.dateOfBirth,
+            dateOfJoining: selectedEmployee.dateOfJoining
+        },
+        validationSchema: editEmployeeSchema,
+        onSubmit: (values) => {
+            console.log(values);
+            axios
+                .put(`${baseUrl}/Employee/${selectedEmployee.employeeId}`, values, {
+                    headers: { 'Authorization': 'Bearer ' + user.token }
+                })
+                .then((response) => {
+                    showSuccessfulToast("Successfully edited employee details!");
+                }).catch((error) => {
+                    showErrorToast("Error editing employee details. Please try again later!");
+                });
+        }
+    })
 
     return (
         <Modal show={showModal} onHide={handleCloseModal}>
@@ -41,45 +44,68 @@ const EditEmployeeModal = ({ showModal, handleCloseModal, selectedEmployee, setS
                 <Modal.Title>Edit details of Employee</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={formik.handleSubmit}>
                     <Form.Group controlId='formName'>
                         <Form.Label>Employee Name</Form.Label>
                         <Form.Control
                             type='text'
+                            className={`form-control ${formik.touched.employeeName && formik.errors.employeeName ? 'is-invalid' : ''
+                                }`}
                             name='employeeName'
-                            value={employee.employeeName}
-                            onChange={handleChange}
+                            value={formik.values.employeeName}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.employeeName && formik.errors.employeeName ? (
+                            <div className="invalid-feedback">{formik.errors.employeeName}</div>
+                        ) : null}
                     </Form.Group>
                     <Form.Group controlId='formDesignation'>
                         <Form.Label>Designation</Form.Label>
                         <Form.Control
                             type='text'
                             name='designation'
-                            value={employee.designation}
-                            onChange={handleChange}
+                            className={`form-control ${formik.touched.designation && formik.errors.designation ? 'is-invalid' : ''
+                                }`}
+                            value={formik.values.designation}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.designation && formik.errors.designation ? (
+                            <div className="invalid-feedback">{formik.errors.designation}</div>
+                        ) : null}
                     </Form.Group>
                     <Form.Group controlId='formDepartment'>
                         <Form.Label>Department</Form.Label>
                         <Form.Control
                             type='text'
                             name='department'
-                            value={employee.department}
-                            onChange={handleChange}
+                            className={`form-control ${formik.touched.department && formik.errors.department ? 'is-invalid' : ''
+                                }`}
+                            value={formik.values.department}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.department && formik.errors.department ? (
+                            <div className="invalid-feedback">{formik.errors.department}</div>
+                        ) : null}
                     </Form.Group>
                     <Form.Group controlId='formGender'>
                         <Form.Label>Gender</Form.Label>
                         <Form.Control
                             as="select"
                             name="gender"
-                            value={employee.gender == 'F' || employee.gender == 'f'  ? 'F': 'M'}
-                            onChange={handleChange}
+                            className={`form-select ${formik.touched.gender && formik.errors.gender ? 'is-invalid' : ''}`}
+                            value={formik.values.gender}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         >
                             <option value={"M"}>Male</option>
                             <option value={"F"}>Female</option>
                         </Form.Control>
+                        {formik.touched.gender && formik.errors.gender ? (
+                            <div className="invalid-feedback">{formik.errors.gender}</div>
+                        ) : null}
                     </Form.Group>
 
                     <Form.Group controlId='formDateOfBirth'>
@@ -87,9 +113,15 @@ const EditEmployeeModal = ({ showModal, handleCloseModal, selectedEmployee, setS
                         <Form.Control
                             type="date"
                             name="dateOfBirth"
-                            value={(employee.dateOfBirth).substring(0, 10)}
-                            onChange={handleChange}
+                            className={`form-control ${formik.touched.dateOfBirth && formik.errors.dateOfBirth ? 'is-invalid' : ''
+                                }`}
+                            value={formik.values.dateOfBirth.substring(0, 10)}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.dateOfBirth && formik.errors.dateOfBirth ? (
+                            <div className="invalid-feedback">{formik.errors.dateOfBirth}</div>
+                        ) : null}
                     </Form.Group>
 
                     <Form.Group controlId='formDateOfJoining'>
@@ -97,11 +129,17 @@ const EditEmployeeModal = ({ showModal, handleCloseModal, selectedEmployee, setS
                         <Form.Control
                             type="date"
                             name="dateOfJoining"
-                            value={(employee.dateOfJoining).substring(0, 10)}
-                            onChange={handleChange}
+                            className={`form-control ${formik.touched.dateOfJoining && formik.errors.dateOfJoining ? 'is-invalid' : ''
+                                }`}
+                            value={formik.values.dateOfJoining.substring(0, 10)}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
+                        {formik.touched.dateOfJoining && formik.errors.dateOfJoining ? (
+                            <div className="invalid-feedback">{formik.errors.dateOfJoining}</div>
+                        ) : null}
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" className='mt-3'>
                         Submit
                     </Button>
                 </Form>
--- frontend/src/Util/validation.js
+++ frontend/src/Util/validation.js
@@ -1,18 +1,18 @@
 import * as yup from "yup";
 
 export const loginObjSchema = yup.object().shape({
-    employeeId: yup.string().required('Employee Id is required!'),
-    password: yup.string().required('Password is required!')
+  employeeId: yup.string().required('Employee Id is required!'),
+  password: yup.string().required('Password is required!')
 })
 
 export const registerSchema = yup.object().shape({
-    password: yup.string().min(3, 'Password must be at least 3 characters').max(15, 'Password can only be atmost 15 characters').required('Password is required!'),
-    confirmPassword: yup.string().required('Confirm Password is required').oneOf([yup.ref('password'), null], 'Passwords must match'),
-    employeeName: yup.string().required('Name is a required field!'),
-    designation: yup.string().required('Designation is a required field!'),
-    department: yup.string().required('Department is a required field!'),
-    gender: yup.string().required('Gender is a required field!'),
-    dateOfBirth: yup.date()
+  password: yup.string().min(3, 'Password must be at least 3 characters').max(15, 'Password can only be atmost 15 characters').required('Password is required!'),
+  confirmPassword: yup.string().required('Confirm Password is required').oneOf([yup.ref('password'), null], 'Passwords must match'),
+  employeeName: yup.string().required('Name is a required field!'),
+  designation: yup.string().required('Designation is a required field!'),
+  department: yup.string().required('Department is a required field!'),
+  gender: yup.string().required('Gender is a required field!'),
+  dateOfBirth: yup.date()
     .required('Date of Birth is required')
     .test(
       'is-valid-age',
@@ -31,5 +31,46 @@ export const registerSchema = yup.object().shape({
         }
         return false;
       }
-    )
+    ),
+})
+
+export const editEmployeeSchema = yup.object().shape({
+  employeeName: yup.string().required('Name is a required field!'),
+  designation: yup.string().required('Designation is a required field!'),
+  department: yup.string().required('Department is a required field!'),
+  gender: yup.string().required('Gender is a required field!'),
+  dateOfBirth: yup.date()
+    .required('Date of Birth is required')
+    .test(
+      'is-valid-age',
+      'Age must be between 18 to 100',
+      function (value) {
+        const today = new Date();
+        const dob = new Date(value);
+        const age = today.getFullYear() - dob.getFullYear();
+        const monthDiff = today.getMonth() - dob.getMonth();
+        if (
+          (age > 18 || (age === 18 && monthDiff >= 0)) &&
+          age <= 100
+        ) {
+          return true;
+        }
+        return false;
+      }
+    ),
+  dateOfJoining: yup.date()
+    .required('Date of Joining is required')
+    .test(
+      'is-valid-join-date',
+      'Date of Joining must be greater than Date of Birth',
+      function (value, context) {
+        const { dateOfBirth } = context.parent;
+        if (dateOfBirth && value) {
+          const dob = new Date(dateOfBirth);
+          const joinDate = new Date(value);
+          return joinDate > dob;
+        }
+        return false;
+      }
+    ),
 })
