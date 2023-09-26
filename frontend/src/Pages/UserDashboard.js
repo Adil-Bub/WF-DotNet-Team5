b03d175fc41eed9505d08458baef81b6a6ba55b6
yachika30 <yachikankumar@gmail.com>
2023-09-27 01:44:29 +0530

    updated user dashboard

--- frontend/src/Pages/UserDashboard.js
+++ frontend/src/Pages/UserDashboard.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { NavBar } from "../Component/LUMANav";
 import Card from 'react-bootstrap/Card';
-import {BsFillFileEarmarkCheckFill} from 'react-icons/bs';
-import {BsFillEyeFill} from 'react-icons/bs';
+import { BsFillFileEarmarkCheckFill } from 'react-icons/bs';
+import { BsFillEyeFill } from 'react-icons/bs';
+import { ImProfile } from 'react-icons/im';
 
 const UserDashboard = () => {
 
@@ -11,12 +12,12 @@ const UserDashboard = () => {
     const user = storedUser ? JSON.parse(storedUser) : null;
     const navigate = useNavigate();
 
-    const navigateToViewLoans= () => {
+    const navigateToViewLoans = () => {
         navigate('/dashboard/user/loans');
     }
 
-    const navigateToApplyLoan = () => {
-        navigate('/dashboard/user/loans/apply');
+    const navigateToMyLoan = () => {
+        navigate('/dashboard/user/my-loans');
     }
 
     const navigateToItemsPurchased = () => {
@@ -25,7 +26,8 @@ const UserDashboard = () => {
     return (
         <>
             <NavBar />
-            <h2 style={{ marginTop: '20px' }}>
+            <div className="text-center">
+                <h2 style={{ marginTop: '20px' }}>
                     User Dashboard
                 </h2>
                 <h4>
@@ -35,7 +37,7 @@ const UserDashboard = () => {
                 <div className="container p-5 text-center fs-5">
                     <div className="d-inline-flex flex-wrap gap-4">
 
-                        <Card style={{ width: '15rem', fontSize: '18px'}}
+                        <Card style={{ width: '15rem', fontSize: '18px' }}
                             border="dark"
                             role="button"
                             className="custom-card"
@@ -48,7 +50,7 @@ const UserDashboard = () => {
                             </Card.Body>
                         </Card>
 
-                        <Card style={{ width: '15rem', fontSize: '18px'}}
+                        <Card style={{ width: '15rem', fontSize: '18px' }}
                             border="dark"
                             role="button"
                             className="custom-card"
@@ -60,9 +62,21 @@ const UserDashboard = () => {
                                 </Card.Text>
                             </Card.Body>
                         </Card>
-
+                        <Card style={{ width: '15rem', fontSize: '18px' }}
+                            border="dark"
+                            role="button"
+                            className="custom-card"
+                            onClick={navigateToMyLoan}>
+                            <Card.Body className="d-flex flex-column align-items-center">
+                                <ImProfile size={100} color="darkcyan" />
+                                <Card.Text className="m-2">
+                                    My Loans
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </div>
                 </div>
+            </div>
         </>
     );
 }
